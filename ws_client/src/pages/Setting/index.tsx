e571d8ffd277925ce2597e137098bd77bc0498a6
黄耀民 <huangyaoming@orbita.com>
2022-03-16 16:48:33 +0800

    开发设置页

--- ws_client/src/pages/Setting/index.tsx
+++ ws_client/src/pages/Setting/index.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
-import { Upload } from 'antd'
-import { PlusOutlined } from '@ant-design/icons'
+import { Upload, Input, Tooltip, Button } from 'antd'
+import { PlusOutlined, InfoCircleOutlined, UserOutlined } from '@ant-design/icons'
 import Ws from 'utils/ws'
 import { WS_SERVICE_URL, CONNECT_TOKEN } from '@/constants/sessionStorage'
 import style from './style/index.module.less'
@@ -38,20 +38,58 @@ function Setting() {
         reader.readAsDataURL(img)
     }
 
+    const exit = () => {
+        window.sessionStorage.removeItem(WS_SERVICE_URL)
+        window.sessionStorage.setItem(CONNECT_TOKEN, 'false')
+    }
+
     return (
         <div className={style['setting']}>
-            <Upload
-                className="avatar-uploader"
-                showUploadList={false}
-                // action="//jsonplaceholder.typicode.com/posts/"
-                onChange={handleChange}
-            >
-                {
-                    imageUrl ?
-                        <img src={imageUrl} alt="" className="avatar" /> :
-                        <PlusOutlined className="avatar-uploader-trigger" />
-                }
-            </Upload>
+            <div className='setting-content'>
+                <p className='setting-title'>请设置你的头像与姓名</p>
+                <Upload
+                    className="avatar-uploader"
+                    showUploadList={false}
+                    // action="//jsonplaceholder.typicode.com/posts/"
+                    onChange={handleChange}
+                >
+                    {
+                        imageUrl ?
+                            <img src={imageUrl} alt="" className="avatar" /> :
+                            <PlusOutlined className="avatar-uploader-trigger" />
+                    }
+                </Upload>
+                <Input
+                    size="large"
+                    placeholder="请输入你的姓名"
+                    prefix={<UserOutlined className="site-form-item-icon" />}
+                    suffix={
+                        <Tooltip title="Extra information">
+                            <InfoCircleOutlined style={{ color: 'rgba(0,0,0,.45)' }} />
+                        </Tooltip>
+                    }
+                />
+                <div className='setting-button'>
+                    <Button
+                        size="large"
+                        type="primary"
+                        danger
+                        style={{
+                            flex: 1,
+                            marginRight: 5
+                        }}
+                        onClick={exit}
+                    >退出连接</Button>
+                    <Button
+                        size="large"
+                        type="primary"
+                        style={{
+                            flex: 1,
+                            marginLeft: 5
+                        }}
+                    >进入连接</Button>
+                </div>
+            </div>
         </div >
     )
 }
--- ws_client/src/route/index.tsx
+++ ws_client/src/route/index.tsx
@@ -1,22 +1,38 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import { BrowserRouter, useRoutes } from 'react-router-dom'
 import { lazy, Suspense } from 'react'
 import Loading from 'components/Loading'
+// import AuthRoute from './AuthRoute'
 
 const Connect = lazy(() => import('pages/Connect'))
 const Setting = lazy(() => import('pages/Setting'))
 const Page404 = lazy(() => import('pages/404'))
 
+const Routes = () => {
+
+  return useRoutes([
+    {
+      path: '/', element: <Connect />
+    },
+    {
+      // path: '/setting', element: <AuthRoute components={<Setting />} redirect='/' />
+      path: '/setting', element: <Setting />
+    },
+    {
+      path: '*', element: <Page404 />
+    },
+  ])
+}
+
 const AppRoutes = () => {
+
   return (
     <BrowserRouter>
       <Suspense fallback={<Loading />}>
-        <Routes>
-          <Route path='/' element={<Connect />} />
-          <Route path='/setting' element={<Setting />} />
-          <Route path='*' element={<Page404 />} />
-        </Routes>
+        <Routes />
       </Suspense>
     </BrowserRouter>
+
   )
 }
 export default AppRoutes
